57ddf7001dbdd6d9bec71a215c2c0f598f11af3c
Naman Narula <naman@narula.dev>
2024-02-05 01:15:52 +0530

    llama.cpp running finally - thanks to rocm (or not?)

---
+++ mistral-basic-run.py
@@ -0,0 +1,39 @@
+from langchain_community.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import (
+    StreamingStdOutCallbackHandler,
+)  # for streaming resposne
+from langchain.llms import OpenAI
+
+
+model_path = "mistral-7b-instruct-v0.1.Q4_0.gguf"
+
+
+template = """Question: {question}
+
+Answer: """
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# Callbacks support token-wise streaming
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+
+n_gpu_layers = -1
+n_batch = 512
+
+llm = LlamaCpp(
+    model_path=model_path,
+    n_gpu_layers=n_gpu_layers,
+    n_batch=n_batch,
+    callback_manager=callback_manager,
+    verbose=False,
+    # temperature=1
+)
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "what's there to eat in new york?"
+
+llm_chain.run(question)
